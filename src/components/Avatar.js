22ba3bfdf17c79602c99c46fd2468573db85dbde
vishwizard <deyamrit959@gmail.com>
2024-09-24 17:03:58 +0530

    Added Navigation Animations

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Avatar(size) {
   return (
-    <div className='size-14 rounded-full overflow-hidden'>
-    <img src={'/1.jpg'} className='shadow-md shadow-gray-500 ' />
-</div>
+    <Link href={"/profile"}>
+      <div className='hover:cursor-pointer size-14 rounded-full overflow-hidden'>
+        <img src={'/1.jpg'} className='shadow-md shadow-gray-500 ' />
+      </div>
+    </Link>
   )
 }
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -3,17 +3,18 @@ import Card from './Card'
 import Avatar from './Avatar'
 import ClickOutHandler from 'react-clickout-handler';
 import { useState } from 'react'
-
+import Link from 'next/link';
 
 export default function PostCard() {
   const [Open, setOpen] = useState(false);
+  const Element = "mt-1 flex gap-3 py-2 hover:scale-105 hover:bg-socialBlue hover:text-white -mx-4 p-4  hover:shadow-md shadow-gray-300 rounded-md transition-all ";
   return (
     <Card>
       <div className="flex gap-3">
         <Avatar></Avatar>
 
         <div className='grow'>
-          <p><a className="font-semibold">Vishal Dey</a> shared a <a className="text-socialBlue font-semibold">Post</a></p>
+          <p><Link href={'/profile'}><span className="font-semibold hover:cursor-pointer hover:underline">Vishal Dey</span> </Link>shared a <a className="text-socialBlue font-semibold">Post</a></p>
           <div><p>2 hours ago</p></div>
         </div>
         <div className='relative'>
@@ -27,23 +28,23 @@ export default function PostCard() {
             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
           </svg>
           
-            <div className='relative'>
+            <div className='relative text-gray-500'>
               {Open && (
                 <div className='bg-white right-0 absolute shadow-md shadow-gray-500 rounded-md border border-gray-100 w-52 p-2'>
-                  <a className='flex p-2 gap-2'>
+                  <a className={Element}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
                     </svg>
                     Save</a>
-                  <a className='flex p-2 gap-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <a className={Element}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                   </svg>
                     Delete</a>
-                  <a className='flex p-2 gap-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <a className={Element}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                   </svg>
                     Hide</a>
-                  <a className='flex p-2 gap-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <a className={Element}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m0-10.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.25-8.25-3.286Zm0 13.036h.008v.008H12v-.008Z" />
                   </svg>
                     Report</a>
